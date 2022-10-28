bbc5c65ac8ecc287fbaf49eba303c019b15277b9
ClausKlein <claus.klein@arcormail.de>
2022-10-28 21:44:07 +0200

    fix build script and add patch

--- build-taox11-for-osx.sh
+++ build-taox11-for-osx.sh
@@ -8,12 +8,13 @@ set -e
 set -u
 
 export LANG=C
-export CC=clang
-export CXX=clang++
+export CC=gcc
+export CXX=g++
 
 export WORKSPACE=$(realpath ..)
 export X11_BASE_ROOT="${WORKSPACE}/axcioma"
 export INSTALL_PREFIX="${X11_BASE_ROOT}/stage"
+rm -rf "${INSTALL_PREFIX}"
 
 source .env_add.sh
 
@@ -26,11 +27,12 @@ export BRIX11_NUMBER_OF_PROCESSORS=6
 rm -rf ciaox11 dancex11
 rm -f ./*.log
 
-"${X11_BASE_ROOT}/bin/brix11" bootstrap taox11
+"${X11_BASE_ROOT}/bin/brix11" bootstrap taox11 || echo ignored
 
 ############################################################
 # patch to build ACE with -std=c++17
-cd "${ACE_ROOT}" && git stash && patch -p2 < ../../ACE_Auto_Ptr.patch
+cd "${ACE_ROOT}" && git stash && patch -N -p2 -i ../../ACE_Auto_Ptr.patch
+cd "${TAOX11_ROOT}" && git stash && patch -N -p2 -i ../taox11-system_exception.patch
 cd "${X11_BASE_ROOT}"
 ############################################################
 
@@ -52,8 +54,7 @@ cd "${X11_BASE_ROOT}"
 # ACE/ACE/include/makeinclude/platform_clang_common.GNU
 # ACE/ACE/include/makeinclude/platform_macosx_common.GNU
 # ACE/ACE/include/makeinclude/platform_macosx.GNU
-echo 'include $(ACE_ROOT)/include/makeinclude/platform_macosx.GNU' >
-${platform_file} > "${ACE_ROOT}/include/makeinclude/platform_macros.GNU"
+echo 'include $(ACE_ROOT)/include/makeinclude/platform_macosx.GNU' > "${ACE_ROOT}/include/makeinclude/platform_macros.GNU"
 
 # ACE/ACE/ace/config.h
 # ACE/ACE/ace/config-macosx.h
@@ -68,7 +69,7 @@ ${platform_file} > "${ACE_ROOT}/include/makeinclude/platform_macros.GNU"
 # ACE/ACE/ace/config-macosx-leopard.h
 echo '#include "ace/config-macosx.h"' > "${ACE_ROOT}/ace/config.h"
 # patch to build ACE with -std=c++20
-echo '#define throw() noexcept' >> "${ACE_ROOT}/ace/config.h"
+#XXX echo '#define throw() noexcept' >> "${ACE_ROOT}/ace/config.h"
 
 # ACE/ACE/bin/MakeProjectCreator/config/default.features
 echo 'ipv6=1' > "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
@@ -85,21 +86,21 @@ perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/examples" -workers
 perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/tests" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
 
 # make all
-make c++20=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} 2>&1 | tee make-all.log
-make c++20=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/orbsvcs/tests" 2>&1 | tee -a make-all.log
-make c++20=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/examples" 2>&1 | tee -a make-all.log
-make c++20=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/tests" 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} 2>&1 | tee make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/orbsvcs/tests" 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/examples" 2>&1 | tee -a make-all.log
+make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/tests" 2>&1 | tee -a make-all.log
 
 # make tests
 "${X11_BASE_ROOT}/bin/brix11" run list -l taox11/bin/taox11_tests.lst -r taox11 2>&1 | tee run-list.log
 
 # install
-make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${X11_BASE_ROOT}" install 2>&1 | tee make-install.log
+#XXX make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${X11_BASE_ROOT}" install 2>&1 | tee make-install.log
 
-find "${INSTALL_PREFIX}/include" -type d -name home -prune -print0 | xargs -0 tree
-find "${INSTALL_PREFIX}/include" -type d -name home -prune -print0 | xargs -0 rm -rf
+#XXX find "${INSTALL_PREFIX}/include" -type d -name home -prune -print0 | xargs -0 tree
+#XXX find "${INSTALL_PREFIX}/include" -type d -name home -prune -print0 | xargs -0 rm -rf
 
 #FIXME: remove the installed include garbage! CK
-rm -rf "${INSTALL_PREFIX}/include"
+#XXX rm -rf "${INSTALL_PREFIX}/include"
 
 exit 0
