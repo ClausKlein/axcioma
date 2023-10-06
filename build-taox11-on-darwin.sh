14e07669e4b631617aaa70f826b8af07b445d772
ClausKlein <claus.klein@arcormail.de>
2023-10-06 20:19:47 +0200

    Use HEAD of master for build [skip ci]
    Add install script
    
    Update and build on Linux
    
    Patch not longer used on CI
    
    Merge branch 'feature/fix-build-on-darwin' of github.com:ClausKlein/axcioma into feature/fix-build-on-darwin
    
    Add example project
    
    Add cmake-format config files
    
    format all cmake files
    
    Add .install.sh
    
    Refactory cmake install script
    
    Refactory CMakeLists.txt
    
    Use TAOX11_ROOT instead of ACE_ROOT_DIR
    
    Set RPATH for executables
    
    Install ridlbe and PerlACE too
    
    Add CMAKE_DEPFILE_FLAGS_CXX setup
    
    Install versionx11.h for rildc
    
    update ci workflow

--- build-taox11-on-darwin.sh
+++ build-taox11-on-darwin.sh
@@ -15,7 +15,7 @@ export WORKSPACE=$(realpath .)
 export X11_BASE_ROOT="${WORKSPACE}"
 export INSTALL_PREFIX="${X11_BASE_ROOT}/stagedir"
 
-source .env_add.sh
+source .envrc
 
 export BRIX11_VERBOSE=1
 export BRIX11_NUMBER_OF_PROCESSORS=6
@@ -30,14 +30,6 @@ set -x
 # see etc/brix11rc
 # and brix11/lib/brix11/brix/common/cmds/bootstrap.rb
 "${X11_BASE_ROOT}/bin/brix11" bootstrap taox11
-
-############################################################
-# patch to build ACE with -std=c++17
-#XXX cd "${ACE_ROOT}" && git stash && patch -N -p2 -i ../../ACE_Auto_Ptr.patch
-#XXX cd "${TAOX11_ROOT}" && git stash && patch -N -p2 -i ../taox11-system_exception.patch
-cd "${X11_BASE_ROOT}"
-############################################################
-
 "${X11_BASE_ROOT}/bin/brix11" configure -W aceroot="${ACE_ROOT}" -W taoroot="${TAO_ROOT}" -W mpcroot="${MPC_ROOT}"
 
 # Print brix11 configuration
@@ -48,17 +40,18 @@ cd "${X11_BASE_ROOT}"
 # gen GNUmakefile from workspace.mwc
 # see taox11/tao/x11/taox11.mpc
 # and ACE/ACE/ace/ace_for_tao.mpc
-"${X11_BASE_ROOT}/bin/brix11" gen build workspace.mwc -- gen build ${TAOX11_ROOT}/examples -- gen build ${TAOX11_ROOT}/orbsvcs/tests -- gen build ${TAOX11_ROOT}/tests
+# FXIME: "${X11_BASE_ROOT}/bin/brix11" gen build workspace.mwc -- gen build ${TAOX11_ROOT}/examples -- gen build ${TAOX11_ROOT}/orbsvcs/tests -- gen build ${TAOX11_ROOT}/tests
 ############################################################
 
-# FIXME: quickfixes for OSX
+# TODO: quickfixes for OSX
 # ACE/ACE/include/makeinclude/platform_gcc_clang_common.GNU
 # ACE/ACE/include/makeinclude/platform_g++_common.GNU
 # ACE/ACE/include/makeinclude/platform_clang_common.GNU
 # ACE/ACE/include/makeinclude/platform_macosx_common.GNU
 # ACE/ACE/include/makeinclude/platform_macosx.GNU
-#XXX echo 'include $(ACE_ROOT)/include/makeinclude/platform_macosx.GNU' > "${ACE_ROOT}/include/makeinclude/platform_macros.GNU"
+platform_file='include $(ACE_ROOT)/include/makeinclude/platform_macosx.GNU'
 
+# create $ACE_ROOT/ace/config.h
 # ACE/ACE/ace/config.h
 # ACE/ACE/ace/config-macosx.h
 # ACE/ACE/ace/config-macosx-mojave.h
@@ -70,24 +63,26 @@ cd "${X11_BASE_ROOT}"
 # ACE/ACE/ace/config-macosx-mountainlion.h
 # ACE/ACE/ace/config-macosx-lion.h
 # ACE/ACE/ace/config-macosx-leopard.h
-#XXX echo '#include "ace/config-macosx.h"' > "${ACE_ROOT}/ace/config.h"
+echo '#include "ace/config-macosx.h"' > "${ACE_ROOT}/ace/config.h"
 
-# patch to build ACE with -std=c++20
-#TODO echo '#define throw() noexcept' >> "${ACE_ROOT}/ace/config.h"
+# create $ACE_ROOT/include/makeinclude/platform_macros.GNU
+echo ${platform_file} > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
+echo CCFLAGS+=-std=c++17 >> ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
 
 # ACE/ACE/bin/MakeProjectCreator/config/default.features
-#XXX echo 'ipv6=1' > "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
-#XXX echo 'versioned_namespace=1' >> "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
+# Create $ACE_ROOT/bin/MakeProjectCreator/config/default.features
+echo 'ipv6=1' > ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
+echo 'versioned_namespace=1' >> ${ACE_ROOT}/bin/MakeProjectCreator/config/default.features
 #XXX echo 'acetaompc=1' >> "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
 #XXX echo 'inline=1' >> "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
 #XXX echo 'optimize=1' >> "${ACE_ROOT}/bin/MakeProjectCreator/config/default.features"
 
 # generate all GNUmakefile's
 # see workspace.mwc
-#XXX perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${X11_BASE_ROOT}/workspace.mwc" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
-#XXX perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/orbsvcs/tests" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
-#XXX perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/examples" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
-#XXX perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/tests" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
+perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${X11_BASE_ROOT}/workspace.mwc" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
+perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/orbsvcs/tests" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
+perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/examples" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
+perl "${TAOX11_ROOT}/bin/mwc.pl" -type gnuace "${TAOX11_ROOT}/tests" -workers ${BRIX11_NUMBER_OF_PROCESSORS}
 
 # make all
 make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${X11_BASE_ROOT}" 2>&1 | tee make-all.log
--- build-taox11-on-linux.sh
+++ build-taox11-on-linux.sh
@@ -11,33 +11,25 @@ export LANG=C
 export CC=gcc
 export CXX=g++
 
-export WORKSPACE=$(realpath ..)
-export X11_BASE_ROOT="${WORKSPACE}/axcioma"
-export INSTALL_PREFIX="${X11_BASE_ROOT}/stage"
-rm -rf "${INSTALL_PREFIX}"
+export WORKSPACE=$(realpath .)
+export X11_BASE_ROOT="${WORKSPACE}"
+export INSTALL_PREFIX="${X11_BASE_ROOT}/stagedir"
 
-source .env_add.sh
-
-set -x
+source .envrc
 
 export BRIX11_VERBOSE=1
 export BRIX11_NUMBER_OF_PROCESSORS=6
 
 # TODO: force to build only taox11! CK
+rm -rf "${INSTALL_PREFIX}"
 rm -rf ciaox11 dancex11
 rm -f ./*.log
+
+set -x
 
 # see etc/brix11rc
 # and brix11/lib/brix11/brix/common/cmds/bootstrap.rb
 "${X11_BASE_ROOT}/bin/brix11" bootstrap taox11
-
-############################################################
-# patch to build ACE with -std=c++17
-cd "${ACE_ROOT}" && git stash && patch -N -p2 -i ../../ACE_Auto_Ptr.patch
-cd "${TAOX11_ROOT}" && git stash && patch -N -p2 -i ../taox11-system_exception.patch
-cd "${X11_BASE_ROOT}"
-############################################################
-
 "${X11_BASE_ROOT}/bin/brix11" configure -W aceroot="${ACE_ROOT}" -W taoroot="${TAO_ROOT}" -W mpcroot="${MPC_ROOT}"
 
 # Print brix11 configuration
@@ -51,11 +43,6 @@ cd "${X11_BASE_ROOT}"
 "${X11_BASE_ROOT}/bin/brix11" gen build workspace.mwc -- gen build "${TAOX11_ROOT}/examples" -- gen build "${TAOX11_ROOT}/orbsvcs/tests" -- gen build "${TAOX11_ROOT}/tests"
 ############################################################
 
-# ACE/ACE/ace/config.h
-# patch to build ACE with -std=c++20
-#TODO echo '#define throw() noexcept' >> "${ACE_ROOT}/ace/config.h"
-
-#NO! "${X11_BASE_ROOT}/bin/brix11" make -N -d ${X11_BASE_ROOT} -- make -N -d ${TAOX11_ROOT}/examples -- make -N -d ${TAOX11_ROOT}/orbsvcs/tests -- make -N -d ${TAOX11_ROOT}/tests 2>&1 | tee make-all.log
 
 # make all
 make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${X11_BASE_ROOT}" 2>&1 | tee make-all.log
@@ -64,7 +51,7 @@ make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/examples" 2>&1
 make c++17=1 -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${TAOX11_ROOT}/tests" 2>&1 | tee -a make-all.log
 
 # make tests
-"${X11_BASE_ROOT}/bin/brix11" run list -l taox11/bin/taox11_tests.lst -r taox11 2>&1 | tee run-list.log
+# TODO "${X11_BASE_ROOT}/bin/brix11" run list -l taox11/bin/taox11_tests.lst -r taox11 2>&1 | tee run-list.log
 
 # install
 make -j ${BRIX11_NUMBER_OF_PROCESSORS} -C "${X11_BASE_ROOT}" install 2>&1 | tee make-install.log
